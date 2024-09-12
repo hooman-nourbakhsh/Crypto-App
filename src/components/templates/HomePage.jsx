aa366b0e143864d9db303e7a86ba68246e117fd5
Mr.Nobody <id4acc2014@protonmail.com>
2024-09-12 13:36:36 +0330

    feat: :sparkles: Added loading state and spinner in TableCoin
    I have introduced a new isLoading state in HomePage.jsx to manage the loading status while fetching data.
    In TableCoin.jsx, I integrated the RotatingLines spinner from the react-loader-spinner library to display a loading indicator while the data is being fetched.
    Once the data is loaded, the table with cryptocurrency details is rendered.

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -4,18 +4,20 @@ import { getCoinList } from "../../services/cryptoApi";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const getData = async () => {
       const response = await fetch(getCoinList());
       const json = await response.json();
       setCoins(json);
+      setIsLoading(false);
     };
     getData();
   }, []);
   return (
     <div>
-      <TableCoin coins={coins} />
+      <TableCoin coins={coins} isLoading={isLoading} />
     </div>
   );
 }
