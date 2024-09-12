ae30894848aa0584c866b3e23ae05c48cc14aa3f
Mr.Nobody <id4acc2014@protonmail.com>
2024-09-12 13:22:27 +0330

    refactor: :recycle: Refactored TableCoin component with TableRow for cleaner structure
    I have refactored the TableCoin.jsx component to improve readability and maintainability.
    The table rows are now separated into a TableRow component, which accepts individual coin data as props.
    This simplifies the code in TableCoin and makes it easier to manage each row's rendering logic.
    The functionality remains the same, including displaying coin details and price change visuals.

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -17,21 +17,7 @@ function TableCoin({ coins }) {
         </thead>
         <tbody>
           {coins.map((coin) => (
-            <tr key={coin.id}>
-              <td>
-                <div>
-                  <img src={coin.image} />
-                  <span>{coin.symbol.toUpperCase()}</span>
-                </div>
-              </td>
-              <td>{coin.name}</td>
-              <td>{coin.current_price.toLocaleString()}</td>
-              <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
-              <td>{coin.total_volume.toLocaleString()}</td>
-              <td>
-                <img src={coin.price_change_percentage_24h > 0 ? chartUp : chartDown} alt={coin.name} />
-              </td>
-            </tr>
+            <TableRow coin={coin} key={coin.id} />
           ))}
         </tbody>
       </table>
@@ -40,3 +26,23 @@ function TableCoin({ coins }) {
 }
 
 export default TableCoin;
+
+const TableRow = ({ coin: { name, image, symbol, current_price, price_change_percentage_24h: price_change, total_volume } }) => {
+  return (
+    <tr>
+      <td>
+        <div>
+          <img src={image} />
+          <span>{symbol.toUpperCase()}</span>
+        </div>
+      </td>
+      <td>{name}</td>
+      <td>{current_price.toLocaleString()}</td>
+      <td>{price_change.toFixed(2)}%</td>
+      <td>{total_volume.toLocaleString()}</td>
+      <td>
+        <img src={price_change > 0 ? chartUp : chartDown} alt={name} />
+      </td>
+    </tr>
+  );
+};
