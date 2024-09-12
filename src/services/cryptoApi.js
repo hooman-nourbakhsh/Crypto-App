d99fe0d56a0631b2f290290439933b59076371ba
Mr.Nobody <id4acc2014@protonmail.com>
2024-09-12 18:17:44 +0330

    feat: :sparkles: Made currency dynamic and added Search component
    I have updated the cryptoApi.js file to make the currency parameter dynamic, allowing it to be set based on user input.
    A new Search.jsx component was created to provide users with an input field and a dropdown menu to select different currencies (USD, EUR, JPY).
    This selection updates the state in HomePage, enabling the display of cryptocurrency prices in the chosen currency.

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,8 @@
 const BASE_URL = "https://api.coingecko.com/api/v3";
 const API_KEY = "CG-ABYsMGwz3bcmCueZPd5QSmFS";
 
-const getCoinList = (page) => {
-  return `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=${page}&x-cg-demo-api-key=${API_KEY}`;
+const getCoinList = (page, currency) => {
+  return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&x-cg-demo-api-key=${API_KEY}`;
 };
 
 export { getCoinList };
