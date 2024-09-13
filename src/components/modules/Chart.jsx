0f6168de97a18a5c6c359c245132c39687df4eb3
Mr.Nobody <id4acc2014@protonmail.com>
2024-09-13 12:21:55 +0330

    feat: :sparkles: I implemented the chart rendering in Chart.jsx using Recharts
    I have implemented the chart rendering in the `Chart.jsx` component using the Recharts library.
    I added a `ChartComponent` that utilizes `LineChart`, `Line`, `CartesianGrid`, `XAxis`, `YAxis`, `Tooltip`, and `Legend` components to display the chart.
    The `convertData` function is used to format the data before rendering.
    I also included a close button to reset the chart state.

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,19 +1,40 @@
 import { useState } from "react";
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+
 import { convertData } from "../../helpers/convertData";
 
 import styles from "./Chart.module.css";
 
 function Chart({ chart, setChart }) {
   const [type, setType] = useState("prices");
-  
+
   return (
     <div className={styles.container}>
       <span className={styles.cross} onClick={() => setChart(null)}>
         X
       </span>
-      <div className={styles.chart}></div>
+      <div className={styles.chart}>
+        <div className={styles.graph}>
+          <ChartComponent data={convertData(chart, type)} type={type} />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Chart;
+
+const ChartComponent = ({ data, type }) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart width={400} height={400} data={data}>
+        <Line type={"monotone"} dataKey={type} stroke="#3874ff" strokeWidth="2px" />
+        <CartesianGrid stroke="#404042" />
+        <YAxis dataKey={type} domain={["auto", "auto"]} />
+        <XAxis dataKey="date" hide />
+        <Legend />
+        <Tooltip />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
